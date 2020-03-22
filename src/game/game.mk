7d508a440f1e6c474d3bfef20649d7e1984d5559
Christian Jäger <mail@plainoldcj.com>
2020-03-22 17:23:38 +0200

    Switched to cmake

--- src/game/game.mk
+++
@@ -1,3 +0,0 @@
-build/project: $(filter build/game/%,$(OBJ)) build/third_party/glew.o build/reflected.o build/game_unit_tests.o
-	@gcc -o $@ $^ $(LIBS)
-	@echo Linking game
--- src/reflect/reflect.mk
+++
@@ -1,2 +0,0 @@
-build/reflect_app: $(filter build/reflect/%,$(OBJ))
-	gcc -o $@ $^ $(LIBS)
